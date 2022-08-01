2136c5b1b561b093624428451cfc192a0625bb25
nschmitt1113 <schmittnikolaus1990@gmail.com>
2022-08-01 11:18:05 -0400

    Added authentication

--- src/utils/firebase.utils.js
+++ src/utils/firebase.utils.js
@@ -1,5 +1,7 @@
 import { initializeApp } from 'firebase/app'
 import { getAuth, signInWithRedirect, signInWithPopup, GoogleAuthProvider  } from 'firebase/auth'
+import { getFirestore, doc, getDoc, setDoc} from 'firebase/firestore'
+
 
 
 const firebaseConfig = {
@@ -22,3 +24,31 @@ const firebaseConfig = {
   export const auth = getAuth();
   export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
   
+
+  export const db = getFirestore();
+  export const createUserDocumentFromAuth = async (userAuth) => {
+    const userDocRef = doc(db, 'users', userAuth.uid);
+
+    console.log(userDocRef);
+
+    const userSnapshot = await getDoc(userDocRef);
+
+    console.log(userSnapshot);
+
+    if(!userSnapshot.exists()){
+      const { displayName, email } = userAuth;
+      const createdAt = new Date();
+
+      try {
+        await setDoc(userDocRef, {
+          displayName,
+          email,
+          createdAt
+        });
+
+      } catch (error) {
+        console.log('error creating the user ', error.message);
+      }
+    }
+    return userDocRef;
+  };
